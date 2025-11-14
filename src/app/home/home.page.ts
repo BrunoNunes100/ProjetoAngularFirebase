f5b5e24dc12846276b3ef375452a84244efd3749
BrunoNunes100 <nunesbrunodeoliveira@gmail.com>
2025-11-14 16:29:59 -0300

    aula 14-11

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -4,6 +4,7 @@ import { CrudService } from '../services/crud.service';
 import { Storage, getDownloadURL, ref, uploadBytesResumable } from '@angular/fire/storage';
 import { MessageService } from '../services/message.service';
 import { Router } from '@angular/router';
+import { APIService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -12,17 +13,14 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-    pokemon: any = {
-      nome: null,
-      poder: null
-};
+  constructor(
+    public apiService: APIService
+  ){
+    this.carregarPaises();
+  }
 
-  constructor(public crudService: CrudService 
-
-   ){ }
-
-
-    enviar(){
-        this.crudService.insert(this.pokemon, 'pokemons')
-    }
+  carregarPaises(){
+    this.apiService.get('name/brazil').subscribe((resp)=>{})
+    
+  }
 }
